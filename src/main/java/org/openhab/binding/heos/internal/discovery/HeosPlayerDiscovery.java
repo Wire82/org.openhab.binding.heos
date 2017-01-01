bf1aa4eff9d6360ed4d347939e3e86c5427ff503
Wire82 <Johannes.Einig@me.com>
2017-01-01 19:44:55 +0100

    Discovery Improvements

--- src/main/java/org/openhab/binding/heos/internal/discovery/HeosPlayerDiscovery.java
+++ src/main/java/org/openhab/binding/heos/internal/discovery/HeosPlayerDiscovery.java
@@ -4,6 +4,8 @@ import static org.openhab.binding.heos.HeosBindingConstants.*;
 
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 import org.eclipse.smarthome.config.discovery.AbstractDiscoveryService;
 import org.eclipse.smarthome.config.discovery.DiscoveryResult;
@@ -12,65 +14,101 @@ import org.eclipse.smarthome.core.thing.ThingUID;
 import org.openhab.binding.heos.HeosBindingConstants;
 import org.openhab.binding.heos.handler.HeosBridgeHandler;
 import org.openhab.binding.heos.resources.HeosPlayer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HeosPlayerDiscovery extends AbstractDiscoveryService {
 
+    private final static int SEARCH_TIME = 20;
+    private final static int INITIAL_DELAY = 5;
+    private final static int SCAN_INTERVAL = 20;
+
+    private Logger logger = LoggerFactory.getLogger(HeosPlayerDiscovery.class);
+
     private HeosBridgeHandler bridge;
+
+    private PlayerScan scanningRunnable;
+
+    private ScheduledFuture<?> scanningJob;
 
     public HeosPlayerDiscovery(HeosBridgeHandler bridge) throws IllegalArgumentException {
         super(Collections.singleton(HeosBindingConstants.THING_TYPE_PLAYER), 20);
         this.bridge = bridge;
+
+        this.scanningRunnable = new PlayerScan();
+
+        // Debug
+        // this.startBackgroundDiscovery();
 
     }
 
     @Override
     protected void startScan() {
 
-        System.out.println("Start Scan for Player");
+        logger.info("Start scan for HEOS Player");
+
         HashMap<String, HeosPlayer> playerMap = new HashMap<>();
         playerMap = bridge.getNewPlayer();
-        System.out.println("Found: " + playerMap.size() + " new Player");
-        createPlayer(playerMap);
+
+        if (playerMap == null) {
+            // Debug
+            System.out.println("Debug: Player Map = Null");
+
+        } else {
+
+            logger.info("Found: {} new Player", playerMap.size());
+            ThingUID bridgeUID = bridge.getThing().getUID();
+
+            for (String playerPID : playerMap.keySet()) {
+                HeosPlayer player = playerMap.get(playerPID);
+                ThingUID uid = new ThingUID(THING_TYPE_PLAYER, playerMap.get(playerPID).getPid());
+                HashMap<String, Object> properties = new HashMap<String, Object>();
+                properties.put(NAME, player.getName());
+                properties.put(PID, player.getPid());
+                properties.put(PLAYER_TYPE, player.getModel());
+                properties.put(HOST, player.getIp());
+                DiscoveryResult result = DiscoveryResultBuilder.create(uid).withLabel(player.getName())
+                        .withProperties(properties).withBridge(bridgeUID).build();
+                thingDiscovered(result);
+            }
+
+        }
 
     }
 
     @Override
     protected void startBackgroundDiscovery() {
 
+        logger.trace("Start HEOS Player background discovery");
+
+        if (scanningJob == null || scanningJob.isCancelled()) {
+            this.scanningJob = AbstractDiscoveryService.scheduler.scheduleWithFixedDelay(this.scanningRunnable,
+                    INITIAL_DELAY, SCAN_INTERVAL, TimeUnit.SECONDS);
+        }
+        logger.trace("scanningJob active");
+
+        // Debug
         System.out.println("start background discover");
 
     }
 
-    private void createPlayer(HashMap<String, HeosPlayer> playerMap) {
+    @Override
+    protected void stopBackgroundDiscovery() {
+        logger.debug("Stop HEOS Player background discovery");
 
-        ThingUID bridgeUID = bridge.getThing().getUID();
-
-        for (String playerPID : playerMap.keySet()) {
-            HeosPlayer player = playerMap.get(playerPID);
-            ThingUID uid = new ThingUID(THING_TYPE_PLAYER, playerMap.get(playerPID).getPid());
-            HashMap<String, Object> properties = new HashMap<String, Object>();
-            properties.put(NAME, player.getName());
-            properties.put(PID, player.getPid());
-            properties.put(PLAYER_TYPE, player.getModel());
-            properties.put(HOST, player.getIp());
-            DiscoveryResult result = DiscoveryResultBuilder.create(uid).withLabel(player.getName())
-                    .withProperties(properties).withBridge(bridgeUID).build();
-            thingDiscovered(result);
+        if (scanningJob != null && !scanningJob.isCancelled()) {
+            scanningJob.cancel(true);
+            scanningJob = null;
         }
-
     }
 
-    // Debug class
+    public class PlayerScan implements Runnable {
 
-    public void debugCreatPlayer() {
-        System.out.println("Debug: Creating Player");
-        ThingUID uid = new ThingUID("heos", "Player", "test");
-        HashMap<String, Object> properties = new HashMap<String, Object>();
-        properties.put(PID, "2514623");
-        properties.put(PLAYER_TYPE, "Heos 7");
-        DiscoveryResult result = DiscoveryResultBuilder.create(uid).withLabel("Player").withProperties(properties)
-                .build();
-        thingDiscovered(result);
+        @Override
+        public void run() {
+            startScan();
+
+        }
 
     }
 
