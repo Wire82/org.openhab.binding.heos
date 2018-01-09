0e6515bedc279da00333ae73b31fa51ec88b8e7d
Johannes Einig <Johannes.Einig@me.com>
2018-01-09 21:24:30 +0100

    new class for ChannelHandlerFactory

---
+++ src/main/java/org/openhab/binding/heos/internal/channelHandler/HeosChannelHandlerVolume.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright (c) 2010-2017 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.heos.internal.channelHandler;
+
+import org.eclipse.smarthome.core.library.types.IncreaseDecreaseType;
+import org.eclipse.smarthome.core.types.Command;
+import org.openhab.binding.heos.handler.HeosBridgeHandler;
+import org.openhab.binding.heos.internal.api.HeosFacade;
+
+/**
+ * This class handles volume changes from the OpenHAb HEOS Volume channel
+ *
+ * @author Johannes Einig - initial contributor
+ *
+ */
+
+public class HeosChannelHandlerVolume extends HeosChannelHandler {
+
+    public HeosChannelHandlerVolume(HeosBridgeHandler bridge, HeosFacade api) {
+        super(bridge, api);
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see
+     * org.openhab.binding.heos.handler.factory.HeosChannelHandler#handleCommandPlayer(org.eclipse.smarthome.core.types.
+     * Command)
+     */
+    @Override
+    protected void handleCommandPlayer(Command command) {
+        if (command instanceof IncreaseDecreaseType) {
+            if (command.equals(IncreaseDecreaseType.INCREASE)) {
+                api.increaseVolume(id);
+            } else {
+                api.decreaseVolume(id);
+            }
+        } else {
+            api.setVolume(command.toString(), id);
+        }
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see
+     * org.openhab.binding.heos.handler.factory.HeosChannelHandler#handleCommandGroup(org.eclipse.smarthome.core.types.
+     * Command)
+     */
+    @Override
+    protected void handleCommandGroup(Command command) {
+        if (command instanceof IncreaseDecreaseType) {
+            if (command.equals(IncreaseDecreaseType.INCREASE)) {
+                api.increaseGroupVolume(id);
+            } else {
+                api.decreaseGroupVolume(id);
+            }
+        } else {
+            api.volumeGroup(command.toString(), id);
+        }
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see
+     * org.openhab.binding.heos.handler.factory.HeosChannelHandler#handleCommandBridge(org.eclipse.smarthome.core.types.
+     * Command)
+     */
+    @Override
+    protected void handleCommandBridge(Command command) {
+        // TODO Auto-generated method stub
+    }
+}
