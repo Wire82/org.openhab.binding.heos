bf098162d25c6566467b519de816f5adebd1cd24
Johannes Einig <Johannes.Einig@me.com>
2018-01-09 21:22:28 +0100

    general rework of class
    channel handling reworked; channelHandlerFactroy is now used. Handler
    now extends the HeosThingBaseHandler;
    Updates now handled by HeosThingBaseHandler

--- src/main/java/org/openhab/binding/heos/handler/HeosGroupHandler.java
+++ src/main/java/org/openhab/binding/heos/handler/HeosGroupHandler.java
@@ -9,10 +9,8 @@
 package org.openhab.binding.heos.handler;
 
 import static org.openhab.binding.heos.HeosBindingConstants.*;
-import static org.openhab.binding.heos.internal.resources.HeosConstants.*;
+import static org.openhab.binding.heos.internal.resources.HeosConstants.GID;
 
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -29,12 +27,12 @@ import org.eclipse.smarthome.core.library.types.StringType;
 import org.eclipse.smarthome.core.thing.ChannelUID;
 import org.eclipse.smarthome.core.thing.Thing;
 import org.eclipse.smarthome.core.thing.ThingStatus;
-import org.eclipse.smarthome.core.thing.binding.BaseThingHandler;
 import org.eclipse.smarthome.core.types.Command;
 import org.eclipse.smarthome.core.types.RefreshType;
+import org.openhab.binding.heos.internal.HeosChannelHandlerFactory;
 import org.openhab.binding.heos.internal.api.HeosFacade;
 import org.openhab.binding.heos.internal.api.HeosSystem;
-import org.openhab.binding.heos.internal.resources.HeosEventListener;
+import org.openhab.binding.heos.internal.channelHandler.HeosChannelHandler;
 import org.openhab.binding.heos.internal.resources.HeosGroup;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -46,20 +44,16 @@ import org.slf4j.LoggerFactory;
  * @author Johannes Einig - Initial contribution
  */
 
-public class HeosGroupHandler extends BaseThingHandler implements HeosEventListener {
+public class HeosGroupHandler extends HeosThingBaseHandler {
 
-    private HeosFacade api;
-    private HeosSystem heos;
     private String gid;
     private HeosGroup heosGroup;
     private HeosBridgeHandler bridge;
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    private Logger logger = LoggerFactory.getLogger(HeosGroupHandler.class);
-
-    public HeosGroupHandler(Thing thing, HeosSystem heos, HeosFacade api) {
-        super(thing);
-        this.heos = heos;
-        this.api = api;
+    public HeosGroupHandler(Thing thing, HeosSystem heos, HeosFacade api,
+            HeosChannelHandlerFactory channelHandlerFactory) {
+        super(thing, heos, api, channelHandlerFactory);
         gid = thing.getConfiguration().get(GID).toString();
         this.heosGroup = new HeosGroup();
         this.heosGroup.setGid(gid);
@@ -72,65 +66,10 @@ public class HeosGroupHandler extends BaseThingHandler implements HeosEventListe
         if (command instanceof RefreshType) {
             return;
         }
-
-        if (channelUID.getId().equals(CH_ID_CONTROL)) {
-            String com = command.toString();
-
-            switch (com) {
-                case "PLAY":
-                    api.play(gid);
-                    break;
-                case "PAUSE":
-                    api.pause(gid);
-                    break;
-                case "NEXT":
-                    api.next(gid);
-                    break;
-                case "PREVIOUS":
-                    api.previous(gid);
-                    break;
-                case "ON":
-                    api.play(gid);
-                    break;
-                case "OFF":
-                    api.pause(gid);
-                    break;
-            }
-        } else if (channelUID.getId().equals(CH_ID_VOLUME)) {
-            api.volumeGroup(command.toString(), gid);
-        } else if (channelUID.getId().equals(CH_ID_MUTE)) {
-            if (command.toString().equals("ON")) {
-                api.muteGroupON(gid);
-            } else {
-                api.muteGroupOFF(gid);
-            }
-        } else if (channelUID.getId().equals(CH_ID_UNGROUP)) {
-            if (command.toString().equals("OFF")) {
-                api.ungroupGroup(gid);
-            } else if (command.toString().equals("ON")) {
-                String[] playerArray = heosGroup.getGroupMemberPidList().toArray(new String[0]);
-                api.groupPlayer(playerArray);
-            }
-        } else if (channelUID.getId().equals(CH_ID_INPUTS)) { // See player handler for description
-            if (bridge.getSelectedPlayer().isEmpty()) {
-                api.playInputSource(gid, command.toString());
-            } else if (bridge.getSelectedPlayer().size() > 1) {
-                logger.warn("Only one source can be selected for HEOS Input. Selected amount of sources: {} ",
-                        bridge.getSelectedPlayer().size());
-                bridge.getSelectedPlayer().clear();
-            } else {
-                for (String sourcePid : bridge.getSelectedPlayer().keySet()) {
-                    api.playInputSource(gid, sourcePid, command.toString());
-                    bridge.getSelectedPlayer().clear();
-                }
-            }
-        } else if (channelUID.getId().equals(CH_ID_PLAY_URL)) {
-            try {
-                URL url = new URL(command.toString());
-                api.playURL(gid, url);
-            } catch (MalformedURLException e) {
-                logger.debug("Command '{}' is not a propper URL. Error: {}", command.toString(), e.getMessage());
-            }
+        HeosChannelHandler channelHandler = channelHandlerFactory.getChannelHandler(channelUID, gid);
+        if (channelHandler != null) {
+            channelHandler.handleCommand(command, gid, this);
+            return;
         }
     }
 
@@ -150,35 +89,15 @@ public class HeosGroupHandler extends BaseThingHandler implements HeosEventListe
     }
 
     @Override
-    public void dispose() {
-        api.unregisterforChangeEvents(this);
-        super.dispose();
-    }
-
-    /**
-     * Plays a media file from an external source. Can be
-     * used for audio sink function
-     *
-     * @param url The external URL where the file is located
-     */
-    public void playURL(String urlStr) {
-        try {
-            URL url = new URL(urlStr);
-            api.playURL(gid, url);
-        } catch (MalformedURLException e) {
-            logger.debug("Command '{}' is not a propper URL. Error: {}", urlStr, e.getMessage());
-        }
-    }
-
     public PercentType getNotificationSoundVolume() {
         return PercentType.valueOf(heosGroup.getLevel());
     }
 
+    @Override
     public void setNotificationSoundVolume(PercentType volume) {
-        api.setVolume(volume.toString(), gid);
+        api.volumeGroup(volume.toString(), gid);
     }
 
-    @SuppressWarnings("null")
     @Override
     public void playerStateChangeEvent(String pid, String event, @Nullable String command) {
         if (this.getThing().getStatus().equals(ThingStatus.UNINITIALIZED)) {
@@ -186,78 +105,15 @@ public class HeosGroupHandler extends BaseThingHandler implements HeosEventListe
                     this.getThing().getStatus().toString());
             return;
         }
-
         if (pid.equals(this.gid)) {
-            if (event.equals(STATE)) {
-                switch (command) {
-                    case PLAY:
-                        updateState(CH_ID_CONTROL, PlayPauseType.PLAY);
-                        break;
-                    case PAUSE:
-                        updateState(CH_ID_CONTROL, PlayPauseType.PAUSE);
-                        break;
-                    case STOP:
-                        updateState(CH_ID_CONTROL, PlayPauseType.PAUSE);
-                        break;
-                }
-            }
-            if (event.equals(VOLUME)) {
-                updateState(CH_ID_VOLUME, PercentType.valueOf(command));
-            }
-            if (event.equals(MUTE)) {
-                switch (command) {
-                    case ON:
-                        this.updateState(CH_ID_MUTE, OnOffType.ON);
-                        break;
-                    case OFF:
-                        updateState(CH_ID_MUTE, OnOffType.OFF);
-                        break;
-                }
-            }
-            if (event.equals(CUR_POS)) {
-                this.updateState(CH_ID_CUR_POS, StringType.valueOf(command));
-            }
-            if (event.equals(DURATION)) {
-                this.updateState(CH_ID_DURATION, StringType.valueOf(command));
-            }
+            handleThingStateUpdate(event, command);
         }
     }
 
-    @SuppressWarnings("null")
     @Override
     public void playerMediaChangeEvent(String pid, HashMap<String, String> info) {
         if (pid.equals(this.gid)) {
-            for (String key : info.keySet()) {
-                switch (key) {
-                    case SONG:
-                        updateState(CH_ID_SONG, StringType.valueOf(info.get(key)));
-                        break;
-                    case ARTIST:
-                        updateState(CH_ID_ARTIST, StringType.valueOf(info.get(key)));
-                        break;
-                    case ALBUM:
-                        updateState(CH_ID_ALBUM, StringType.valueOf(info.get(key)));
-                        break;
-                    case IMAGE_URL:
-                        updateState(CH_ID_IMAGE_URL, StringType.valueOf(info.get(key)));
-                        break;
-                    case STATION:
-                        if (info.get(SID).equals(INPUT_SID)) {
-                            String inputName = info.get(MID).substring(info.get(MID).indexOf("/") + 1);
-                            updateState(CH_ID_INPUTS, StringType.valueOf(inputName));
-                        }
-                        updateState(CH_ID_STATION, StringType.valueOf(info.get(key)));
-                        break;
-                    case TYPE:
-                        updateState(CH_ID_TYPE, StringType.valueOf(info.get(key)));
-                        if (info.get(key).equals(STATION)) {
-                            updateState(CH_ID_STATION, StringType.valueOf(info.get(STATION)));
-                        } else {
-                            updateState(CH_ID_STATION, StringType.valueOf("No Station"));
-                        }
-                        break;
-                }
-            }
+            handleThingMediaUpdate(info);
         }
     }
 
@@ -276,12 +132,27 @@ public class HeosGroupHandler extends BaseThingHandler implements HeosEventListe
         heosGroup.setGroupMemberPidList(memberPidList);
     }
 
+    /**
+     * Sets the status of the HEOS group to OFFLINE.
+     * Also sets the UNGROUP channel to OFF and the CONTROL
+     * channel to PAUSE
+     */
+    @Override
     public void setStatusOffline() {
         api.unregisterforChangeEvents(this);
         updateState(CH_ID_STATUS, StringType.valueOf(OFFLINE));
         updateState(CH_ID_UNGROUP, OnOffType.OFF);
         updateState(CH_ID_CONTROL, PlayPauseType.PAUSE);
         updateStatus(ThingStatus.OFFLINE);
+    }
+
+    /**
+     *
+     * @return The instance of the HEOS group
+     */
+
+    public HeosGroup getHeosGroup() {
+        return heosGroup;
     }
 
     public class InitializationRunnable implements Runnable {
