032e8b7f95af09b883af43825a9e9841b709a068
Johannes Einig <Johannes.Einig@me.com>
2018-03-18 21:20:03 +0100

    code simplification and documentation improved

--- src/main/java/org/openhab/binding/heos/internal/resources/MyEventListener.java
+++ src/main/java/org/openhab/binding/heos/internal/resources/MyEventListener.java
@@ -12,8 +12,11 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
- * The {@link MyEventListener } is an Event Listener for
- * String messages
+ * The {@link MyEventListener } is used for classes which
+ * wants to inform players or groups about change events
+ * from the HEOS system. Classes which wants to be informed
+ * has to implement the {@link HeosEventListener} and register at
+ * the class which extends this {@link MyEventListener} *
  *
  * @author Johannes Einig - Initial contribution
  */
@@ -21,29 +24,59 @@ import java.util.HashMap;
 public class MyEventListener {
     protected ArrayList<HeosEventListener> listenerList = new ArrayList<HeosEventListener>();
 
+    /**
+     * Register a listener from type {@link HeosEventListener} to be notified by
+     * a change event
+     *
+     * @param listener the lister from type {@link HeosEventListener} for change events
+     */
+
     public void addListener(HeosEventListener listener) {
         listenerList.add(listener);
     }
+
+    /**
+     * Removes the listener from the notification list
+     *
+     * @param listener the listener from type {@link HeosEventListener} to be removed
+     */
 
     public void removeListener(HeosEventListener listener) {
         listenerList.remove(listener);
     }
 
+    /**
+     * Notifies the registered listener of a changed state type event
+     *
+     * @param pid the ID of the player or group which has changed
+     * @param event the name of the event (see {@link HeosConstants} for event types)
+     * @param command the command of the event
+     */
+
     public void fireStateEvent(String pid, String event, String command) {
-        for (int i = 0; i < listenerList.size(); i++) {
-            listenerList.get(i).playerStateChangeEvent(pid, event, command);
-        }
+        listenerList.forEach(element -> element.playerStateChangeEvent(pid, event, command));
     }
+
+    /**
+     * Notifies the registered listener of a changed media type event
+     * 
+     * @param pid the ID of the player or group which has changed
+     * @param info an HashMap which contains the media information
+     */
 
     public void fireMediaEvent(String pid, HashMap<String, String> info) {
-        for (int i = 0; i < listenerList.size(); i++) {
-            listenerList.get(i).playerMediaChangeEvent(pid, info);
-        }
+        listenerList.forEach(element -> element.playerMediaChangeEvent(pid, info));
     }
 
+    /**
+     * Notifies the registered listener if a change of the bridge state
+     * 
+     * @param event the event type
+     * @param result the result (success or fail)
+     * @param command the command of the event
+     */
+
     public void fireBridgeEvent(String event, String result, String command) {
-        for (int i = 0; i < listenerList.size(); i++) {
-            listenerList.get(i).bridgeChangeEvent(event, result, command);
-        }
+        listenerList.forEach(element -> element.bridgeChangeEvent(event, result, command));
     }
 }
